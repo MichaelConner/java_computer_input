5b42470377b117d4812c0883083fedae2d1a6e59
Michael Conner <michaelconner@hotmail.co.uk>
2019-02-20 14:45:31 +0000

    MVP completed

---
+++ src/main/java/device_management/Computer.java
@@ -0,0 +1,52 @@
+package device_management;
+
+import behaviours.IOutput;
+import behaviours.IInput;
+
+public class Computer {
+
+    private int ram;
+    private int hddSize;
+
+    private IOutput outputDevice;
+    private IInput inputDevice;
+
+    public Computer(int ram, int hddSize, IOutput outputDevice, IInput inputDevice) {
+        this.ram = ram;
+        this.hddSize = hddSize;
+        this.outputDevice = outputDevice;
+        this.inputDevice = inputDevice;
+    }
+
+    public int getRam() {
+        return this.ram;
+    }
+
+    public int getHddSize() {
+        return this.hddSize;
+    }
+
+    public IOutput getOutputDevice() {
+        return this.outputDevice;
+    }
+
+    public String outputData(String data) {
+        return this.outputDevice.outputData(data);
+    }
+
+    public void setOutputDevice(IOutput outputDevice){
+        this.outputDevice = outputDevice;
+    }
+
+    public IInput getInputDevice() {
+        return this.inputDevice;
+    }
+
+    public String inputData() {
+        return this.inputDevice.sendData();
+    }
+
+    public void setInputDevice(IInput inputDevice){
+        this.inputDevice = inputDevice;
+    }
+}
